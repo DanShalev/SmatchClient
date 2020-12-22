95731b80cb9c973a8d3d7ac937bf737ab56df2a0
Dan Shalev <shalev444@gmail.com>
2020-12-22 22:55:49 +0000

    Profile Refactoring

--- src/components/swipe/Profiles.js
+++ src/components/swipe/Profiles.js
@@ -7,121 +7,121 @@ import Card from "./Card";
 import { initAnimationProps, initAnimation, generateAnimationParams } from "../utils/swipingAnimationUtils";
 
 export default function Profiles({ profilesProp }) {
-  function functionConstructor() {
-    if (!props.current) {
-      props.current = initAnimationProps();
-      props.current.onGestureEvent = Animated.event(
-        [
-          {
-            nativeEvent: {
-              translationX: props.current.translationX,
-              translationY: props.current.translationY,
-              velocityX: props.current.velocityX,
-              state: props.current.gestureState,
-            },
-          },
-        ],
-        { useNativeDriver: true }
-      );
-    }
-  }
+  const [profiles, setProfiles] = useState(profilesProp);
+  const props = useRef(); // Saves props once for all the times we re-render Profiles class (while using useState)
 
-  const swipingEventTrigger = ([translationX]) => {
+  initProps(props);
+  props.current = initAnimation(props.current, swipingEventTrigger(profiles, setProfiles), setProfiles);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <TopButtons />
+      <ProfileCards profiles={profiles} props={props.current} />
+      <BottomButtons profiles={profiles} />
+    </SafeAreaView>
+  );
+}
+
+function swipingEventTrigger(profiles, setProfiles) {
+  return ([translationX]) => {
     const [lastProfile, ...restOfProfiles] = profiles;
 
     // Check user action (likes/noped profile card)
     let liked = translationX > 0;
     Alert.alert(`You ${liked ? "liked" : "noped"} ${lastProfile.name}!`);
     setProfiles(restOfProfiles);
-  };
+  }
+}
 
-  function renderTopButtons() {
-    return (
-      <View style={styles.header}>
-        <Icon
-          name="user"
-          size={nonCssStyles.icons.iconSize}
-          color={nonCssStyles.icons.topIconColor}
-          onPress={() => {
-            Alert.alert("Profile Button Pressed");
-          }}
-        />
-        <Icon
-          name="message-circle"
-          size={nonCssStyles.icons.iconSize}
-          color={nonCssStyles.icons.topIconColor}
-          onPress={() => {
-            Alert.alert("Message Box");
-          }}
-        />
-      </View>
+function initProps(props) {
+  if (!props.current) {
+    props.current = initAnimationProps();
+    props.current.onGestureEvent = Animated.event(
+      [
+        {
+          nativeEvent: {
+            translationX: props.current.translationX,
+            translationY: props.current.translationY,
+            velocityX: props.current.velocityX,
+            state: props.current.gestureState,
+          },
+        },
+      ],
+      { useNativeDriver: true }
     );
   }
+}
 
-  function renderProfiles() {
-    // Render Component
-    const { likeOpacity, nopeOpacity, style } = generateAnimationParams(props.current);
-    const { onGestureEvent } = props.current;
-    const [lastProfile, ...restOfProfiles] = profiles;
-    return (
-      lastProfile && (
-        <View style={styles.cards}>
-          {restOfProfiles.reverse().map((profile) => (
-            <Card key={profile.id} {...{ profile }} />
-          ))}
-          <PanGestureHandler onHandlerStateChange={onGestureEvent} {...{ onGestureEvent }}>
-            <Animated.View {...{ style }}>
-              <Card profile={lastProfile} {...{ likeOpacity, nopeOpacity }} />
-            </Animated.View>
-          </PanGestureHandler>
-        </View>
-      )
-    );
-  }
-
-  function renderBottomButtons() {
-    const [lastProfile, ...restOfProfiles] = profiles;
-    return (
-      lastProfile && (
-        <View style={styles.footer}>
-          <View style={styles.circle}>
-            <Icon
-              name="x"
-              size={nonCssStyles.icons.iconSize}
-              color={nonCssStyles.icons.nopeIconColor}
-              onPress={() => {
-                Alert.alert("Nope Pressed");
-              }}
-            />
-          </View>
-          <View style={styles.circle}>
-            <Icon
-              name="heart"
-              size={nonCssStyles.icons.iconSize}
-              color={nonCssStyles.icons.likeIconColor}
-              onPress={() => {
-                Alert.alert("Like Pressed");
-              }}
-            />
-          </View>
-        </View>
-      )
-    );
-  }
-
-  const [profiles, setProfiles] = useState(profilesProp);
-  const props = useRef(); // Saves props once for all the times we re-render Profiles class (while using useState)
-
-  functionConstructor();
-  props.current = initAnimation(props.current, swipingEventTrigger);
-  const profileCards = renderProfiles();
-
+function ProfileCards({ profiles, props }) {
+  const { likeOpacity, nopeOpacity, style } = generateAnimationParams(props);
+  const { onGestureEvent } = props;
+  const [lastProfile, ...restOfProfiles] = profiles;
   return (
-    <SafeAreaView style={styles.container}>
-      {renderTopButtons()}
-      {profileCards}
-      {renderBottomButtons()}
-    </SafeAreaView>
+    lastProfile && (
+      <View style={styles.cards}>
+        {restOfProfiles.reverse().map((profile) => (
+          <Card key={profile.id} {...{ profile }} />
+        ))}
+        <PanGestureHandler onHandlerStateChange={onGestureEvent} {...{ onGestureEvent }}>
+          <Animated.View {...{ style }}>
+            <Card profile={lastProfile} {...{ likeOpacity, nopeOpacity }} />
+          </Animated.View>
+        </PanGestureHandler>
+      </View>
+    )
+  );
+}
+
+function TopButtons() {
+  return (
+    <View style={styles.header}>
+      <Icon
+        name="user"
+        size={nonCssStyles.icons.iconSize}
+        color={nonCssStyles.icons.topIconColor}
+        onPress={() => {
+          Alert.alert("Profile Button Pressed");
+        }}
+      />
+      <Icon
+        name="message-circle"
+        size={nonCssStyles.icons.iconSize}
+        color={nonCssStyles.icons.topIconColor}
+        onPress={() => {
+          Alert.alert("Message Box");
+        }}
+      />
+    </View>
+  );
+}
+
+function BottomButtons({ profiles }) {
+  const [lastProfile] = profiles;
+  return (
+    lastProfile && (
+      <View style={styles.footer}>
+        <View style={styles.circle}>
+          <Icon
+            name="x"
+            size={nonCssStyles.icons.iconSize}
+            color={nonCssStyles.icons.nopeIconColor}
+            onPress={() => {
+              Alert.alert("Nope Pressed");
+            }}
+          />
+        </View>
+        <View style={styles.circle}>
+          <Icon
+            name="heart"
+            size={nonCssStyles.icons.iconSize}
+            color={nonCssStyles.icons.likeIconColor}
+            onPress={() => {
+              Alert.alert("Like Pressed");
+            }}
+          />
+        </View>
+      </View>
+    )
   );
 }
 
