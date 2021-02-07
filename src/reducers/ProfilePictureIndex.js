6516787ac7e17662776863001b675975c91e5a3e
itaypeled <123456789>
2021-02-07 19:21:29 +0200

    fix 1
    Signed-off-by: itaypeled <123456789>

--- src/reducers/ProfilePictureIndex.js
+++ src/reducers/ProfilePictureIndex.js
@@ -4,14 +4,14 @@ const initialState = {
   profilePictureIndex: 0,
 };
 
-const setProfilePicIndex = (state, action) => {
-  return Object.assign({}, state, {profilePictureIndex: action.payload});
-}
 
 export default function profilePictureIndex(state = initialState, action) {
   switch (action.type) {
     case PROFILE_INDEX:
-      return setProfilePicIndex(state, action)
+      return {
+        ...state,
+        profilePictureIndex: action.payload
+      };
     default:
       return state;
   }
