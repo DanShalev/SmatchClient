088fd3b571f7580c660707acbab8673474236c23
itaypeled <123456789>
2021-02-08 19:33:29 +0200

    Modal redesign
    Signed-off-by: itaypeled <123456789>

--- src/components/swipe/MatchModal.js
+++ src/components/swipe/MatchModal.js
@@ -1,62 +1,78 @@
 import Modal from "react-native-modal";
-import {Image, Pressable, StyleSheet, Text, View} from "react-native";
-import React, {useState} from "react";
+import {Image, ImageBackground, Pressable, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import React from "react";
 
 export function MatchModal({matchProfileImage, isVisible, onSwipeComplete}) {
     return (
-        <Modal isVisible={isVisible}
-               hasBackdrop={true}
-               backdropColor={"black"}
-               backdropOpacity={0.3}
-               animationInTiming={500}
-               onSwipeComplete={onSwipeComplete}
-               swipeDirection={["left", "right", "down", "up"]}>
-            <Pressable style={styles.modal} onPress={onSwipeComplete}>
-                <View style={styles.smatchTextRow}>
-                    <Text style={styles.smatchText}> It's a Smatch!</Text>
-                </View>
-                <View style={styles.modalImagesRow}>
-                    <Image style={styles.modalImage} source={{uri: matchProfileImage}}/>
-                    <View style={styles.modalUserImage}>
-                        <Text>User picture</Text>
-                    </View>
-                </View>
-            </Pressable>
-        </Modal>
+      <Modal isVisible={isVisible}
+             hasBackdrop={true}
+             backdropColor={"black"}
+             backdropOpacity={0.3}
+             animationInTiming={500}
+             onSwipeComplete={onSwipeComplete}
+             swipeDirection={["left", "right", "down", "up"]}>
+          <Pressable style={styles.modal} onPress={onSwipeComplete}>
+              <ImageBackground style={styles.backgroundImage} imageStyle={{borderRadius: 15}}
+                               source={require("../../../assets/matchBackground.jpg")}>
+                  <View style={styles.smatchTextRow}>
+                      <Image style={styles.smatchTextImage}
+                             source={require("../../../assets/matchLogoGrey.png")}/>
+                  </View>
+                  <View style={styles.modalImagesRow}>
+                      <Image style={styles.modalImage} source={{uri: matchProfileImage}}/>
+                      {/*TODO replace mock with user image*/}
+                      <Image style={styles.modalImage} source={require("../../../mocks/images/swipe/3.jpg")}/>
+                  </View>
+                  <TouchableOpacity style={styles.messageBox}>
+                      <Text style={styles.messageText}>Send a message</Text>
+                  </TouchableOpacity>
+              </ImageBackground>
+          </Pressable>
+      </Modal>
     );
 }
 
 const styles = StyleSheet.create({
+    backgroundImage: {
+        flex: 1,
+    },
+    smatchTextImage: {
+        width: 300,
+        height: 110,
+        borderRadius: 15
+    },
+    messageText: {
+        fontWeight: "bold",
+        paddingTop: 9,
+        color: "white",
+        textAlign: "center",
+    },
+    messageBox: {
+        alignSelf: "center",
+        width: 135,
+        height: 40,
+        backgroundColor: "tomato",
+        borderRadius: 30,
+        marginTop: 40
+    },
     modal: {
-        flex: 0.35,
-        alignContent: "center",
-        backgroundColor: "whitesmoke",
-        borderRadius: 15,
+        flex: 0.60,
     },
     modalImage: {
         width: 100,
         height: 100,
         borderRadius: 90,
-    },
-    modalUserImage: {
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "burlywood",
-        width: 100,
-        height: 100,
-        borderRadius: 90,
+        borderWidth: 1,
+        borderColor: "whitesmoke",
     },
     smatchTextRow: {
         flexDirection: "row",
         alignSelf: "center",
-        paddingTop: 10,
-        marginBottom: 30,
-    },
-    smatchText: {
-        fontSize: 35,
+        marginTop: 20,
     },
     modalImagesRow: {
         flexDirection: "row",
-        justifyContent: "space-around",
+        justifyContent: "space-evenly",
+        marginTop: 25,
     }
 });
