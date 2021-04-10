20130b3f9e91ced59ddfd2a522fded27d760c001
guy <guymosh5@gmail.com>
2021-04-10 14:06:46 +0300

    Renaming GroupMatches

--- src/redux/reducers/MatchesReducer.js
+++ src/redux/reducers/MatchesReducer.js
@@ -1,4 +1,4 @@
-import {ADD_MATCH, RESET_MATCHES} from "../actions/actions";
+import { ADD_MATCH, RESET_MATCHES } from "../actions/actions";
 
 const initialState = {
   matches: {},
@@ -7,35 +7,35 @@ const initialState = {
 const matchesReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_MATCH:
-      let currentGroup = state.matches[action.payload.groupId];
-      if (!currentGroup) {
-        currentGroup = []
+      let currentGroupMatches = state.matches[action.payload.groupId];
+      if (!currentGroupMatches) {
+        currentGroupMatches = [];
         return {
           ...state,
           matches: {
             ...state.matches,
-            [action.payload.groupId]: [action.payload.userProfile]
-          }
-        }
+            [action.payload.groupId]: [action.payload.userProfile],
+          },
+        };
       }
-      if (currentGroup.some((e) => e.id === action.payload.userProfile.id)) {
+      if (currentGroupMatches.some((e) => e.id === action.payload.userProfile.id)) {
         return state;
       }
       return {
         ...state,
         matches: {
           ...state.matches,
-          [action.payload.groupId]: [...currentGroup, action.payload.userProfile]
-        }
-      }
+          [action.payload.groupId]: [...currentGroupMatches, action.payload.userProfile],
+        },
+      };
     case RESET_MATCHES:
       return {
         ...state,
-        matches: {}
-      }
+        matches: {},
+      };
     default:
       return state;
   }
-}
+};
 
 export default matchesReducer;
