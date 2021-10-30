8da51d1db273a13878fdd3bc64e11db04711c31e
itay <peleditay@gmail.com>
2021-10-30 16:46:48 +0300

    Added AWS EC2 to SmatchServer.js

--- src/api/SmatchServer.js
+++ src/api/SmatchServer.js
@@ -1,21 +1,8 @@
-import Axios from 'axios';
+import Axios from "axios";
 
-/*
-* TODO: In order to test your server connection, do one of the following:
-*  1. Run your java server. You are probably running it on localhost:8080.
-*  2.1 Check your computer ip (so the client phone will know how to access it):
-*     "http://127.0.0.8:8080"
-*  2.2 Alternatively (to step 2.1 only), you can install ngrok and use it:
-*       3.1 I already installed the module as a dev dependency.
-*           Just open terminal in this project root and run "npm run tunnel".
-*       3.2 Note that this is an alias, in the background it actually runs "ngrok http 8080".
-*           This command will share your 8080 port (java server) into the url printed by ngrok.
-*       3.4 The output is a URL of the following type "http://3efed74dbba0.ngrok.io", use it.
-*           Keep in mind that every 8 hours you will need to reran ngrok and change the link here in order to use it.
-* */
 
 const smatchServer = Axios.create({
-    baseURL: 'http://192.168.0.101:8080'
+  baseURL: "http://ec2-18-188-126-103.us-east-2.compute.amazonaws.com:8080"
 });
 
 export default smatchServer;
